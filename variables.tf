075503ef85ffa2cd1fe6e5de3ca666b9fccd9be0
Anilkumar7980 <117730085+Anilkumar7980@users.noreply.github.com>
2024-02-12 14:53:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,18 +48,4 @@ variable "gke_node_type" {
 
 variable "google_application_credentials" {
   type = string
-
-  default <<EOT
-{
-  "type": "service_account",
-  "project_id": "cicd-project-412206",
-  "private_key_id": "d7cc05fe2e8b0a5e29204426f25ffb4ca99e172f",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC8kkPBhGl62/4m\ns6DFtWhUK+/nI7+W1i2TlSqSljSb1AalH3McfiNr7BOTnCX2kqyhI+ObTrC0uOw1\n4u7ZupbC19tswp8OAoJISV8Ij74EDTtqZ1ERSG9danvFqH4CiTWw3P+WfB+athyB\n73fKmlbBBbU9eE0xbJ6K6DE4cjVu2Y/zINxrEWlgG5lgTJpZujBMLmVW4HI6rjf9\nQD2aFU59427MH6Ymv8LDBwXsU3Zf6P18Di4HobuPAJTdxKL4rgxvU7bVGwzpDlut\noGi0bsEPtvl8pktUsqwcueek1aqFNCdoDpWGxcc9ba76ZUz+JNzjR0fD36ysREOn\nxqq5bvA9AgMBAAECggEADKSZ2l2wXMZnMd0VSGwzRIF1oj+pVsmhYKW9pvpfKJt8\nnpGPP..."
-  "client_email": "my-service-account@cicd-project-412206.iam.gserviceaccount.com"
 }
-EOT
-
-  description = "Service account credentials for Google Cloud Platform access"
-}
-
-
